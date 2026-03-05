2a9d710e664bb1d22e2a9b6cac82cd3433811fe9
Jonas Reif <jonas@reifcloud.de>
2026-03-05 09:57:41 +0100

    solve part one of the task

--- src/calculator.py
+++ src/calculator.py
@@ -1,2 +1,8 @@
 def add(a, b):
     return a + b
+
+def mul(a, b):
+    raise NotImplementedError
+
+def div(a, b):
+    raise NotImplementedError
--- src/test/test_calculator.py
+++ src/test/test_calculator.py
@@ -2,7 +2,7 @@
 This is the calculator test file
 """
 
-from calculator import add
+from calculator import add, mul, div
 
 
 # this is an example
@@ -12,3 +12,14 @@ def test_add():
 
 
 # add your tests here
+
+def test_mul():
+    assert mul(0, 0) == 0, "result not correct"
+    assert mul(2, 0) == 0, "result not correct"
+    assert mul(2, 3) == 6, "result not correct"
+
+def test_div():
+    assert div(0, 1) == 0, "result not correct"
+    assert div(2, 1) == 2, "result not correct"
+    assert div(6, 3) == 2, "result not correct"
+    assert div(4, 8) == 0.5, "result not correct"
